b2b65bb0a2ff9f626e8e31fc22751d73ee14224e
Pablo <pablo.garcia1004@gmail.com>
2025-01-03 19:08:39 +0000

    feat(api): Procesar datos de la dirección del viento

--- src/tiempo.js
+++ src/tiempo.js
@@ -19,3 +19,32 @@ const temperatura = `${respuesta.current_weather.temperature} ${respuesta.curren
 const velocidadViento = `${respuesta.current_weather.windspeed} ${respuesta.current_weather_units.windspeed}`;
 const direccionVientoGrados = respuesta.current_weather.winddirection;
 const codigoTiempo = respuesta.current_weather.weathercode;
+
+// Procesamiento de datos de dirección del viento
+function procesaDireccionViento(direccionVientoGrados) {
+  const grados = Number(direccionVientoGrados);
+
+  var direccion = "";
+
+  if (grados >= 337.5 || grados < 22.5) {
+    direccion = "Norte";
+  } else if (grados >= 22.5 && grados < 67.5) {
+    direccion = "Noreste";
+  } else if (grados >= 67.5 && grados < 112.5) {
+    direccion = "Este";
+  } else if (grados >= 112.5 && grados < 157.5) {
+    direccion = "Sureste";
+  } else if (grados >= 157.5 && grados < 202.5) {
+    direccion = "Sur";
+  } else if (grados >= 202.5 && grados < 247.5) {
+    direccion = "Suroeste";
+  } else if (grados >= 247.5 && grados < 292.5) {
+    direccion = "Oeste";
+  } else if (grados >= 292.5 && grados < 337.5) {
+    direccion = "Noroeste";
+  }
+
+  return direccion;
+}
+
+const direccionViento = `${direccionVientoGrados}${respuesta.current_weather_units.winddirection} ${procesaDireccionViento(direccionVientoGrados)}`;

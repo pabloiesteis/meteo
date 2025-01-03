6c5c0a73efaf64967bd1adac29aabcdb2fe0e700
Pablo <pablo.garcia1004@gmail.com>
2025-01-03 19:09:39 +0000

    feat(api): Procesar códigos de tiempo

--- src/tiempo.js
+++ src/tiempo.js
@@ -48,3 +48,67 @@ function procesaDireccionViento(direccionVientoGrados) {
 }
 
 const direccionViento = `${direccionVientoGrados}${respuesta.current_weather_units.winddirection} ${procesaDireccionViento(direccionVientoGrados)}`;
+
+// Procesamiento de codigos de tiempo
+function procesaCodigoTiempo(codigoTiempo) {
+  switch (codigoTiempo) {
+    case 0:
+      console.log("☀️ Cielo despejado");
+      break;
+    case 1:
+    case 2:
+    case 3:
+      console.log(
+        "🌤️ Principalmente despejado, parcialmente nublado y cubierto"
+      );
+      break;
+    case 45:
+    case 48:
+      console.log("🌫️ Niebla y niebla con escarcha");
+      break;
+    case 51:
+    case 53:
+    case 55:
+      console.log("🌧️ Llovizna: Intensidad ligera, moderada y densa");
+      break;
+    case 56:
+    case 57:
+      console.log("❄️🌧️ Llovizna helada: Intensidad ligera y densa");
+      break;
+    case 61:
+    case 63:
+    case 65:
+      console.log("🌧️ Lluvia: Intensidad ligera, moderada y fuerte");
+      break;
+    case 66:
+    case 67:
+      console.log("❄️🌧️ Lluvia helada: Intensidad ligera y fuerte");
+      break;
+    case 71:
+    case 73:
+    case 75:
+      console.log("❄️ Caída de nieve: Intensidad ligera, moderada y fuerte");
+      break;
+    case 77:
+      console.log("🌨️ Granizos de nieve");
+      break;
+    case 80:
+    case 81:
+    case 82:
+      console.log("🌦️ Chubascos: Intensidad ligera, moderada y violenta");
+      break;
+    case 85:
+    case 86:
+      console.log("❄️🌨️ Chubascos de nieve: Intensidad ligera y fuerte");
+      break;
+    case 95:
+      console.log("⛈️ Tormenta: Intensidad ligera o moderada");
+      break;
+    case 96:
+    case 99:
+      console.log("⛈️🌩️ Tormenta con granizo: Intensidad ligera y fuerte");
+      break;
+    default:
+      console.log("❓ Código de tiempo no reconocido");
+  }
+}
